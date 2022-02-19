ab9f7093e0725cbd2b32070d55e4923ee0c95431
ridho <sugita.ridho@gmail.com>
2022-02-19 19:42:14 +0700

    Update app.js

--- app.js
+++ app.js
@@ -89,8 +89,8 @@ const db = require('./helpers/db.js');
           //msg.reply(pesan); 
          // pesan = "Jadwal Shalat Pekanbaru\n"+"Tanggal : "+res.data.query.tanggal+"\n"+"imsak : "+res.data.jadwal.data.imsak+"\n"+"terbit : "++res.data.jadwal.data.terbit+"\n"+"subuh : "++res.data.jadwal.data.subuh+"\n"+"dhuha : "++res.data.jadwal.data.dhuha+"\n"+"dzuhur : "++res.data.jadwal.data.dzuhur+"\n"+"ashar : "++res.data.jadwal.data.ashar+"\n"+"maghrib : "++res.data.jadwal.data.maghrib+"\n"+"isya : "++res.data.jadwal.data.isya+"\n"
            //  msg.reply(pesan);
-          msg.reply("OK"+JSON.stringify(res.data.update.penambahan.jumlah_positif)); 
-        //  msg.reply(res); 
+          msg.reply("Data COVID Indonesia per "+JSON.stringify(res.data.update.penambahan.tanggal))+"\nSumber covid19.go.id\n\nJumlah Positif"+JSON.stringify(res.data.update.penambahan.jumlah_positif))+"\nJumlah Meninggal: "+JSON.stringify(res.data.update.penambahan.jumlah_meninggal))+"\nJumlah Sembuh: "+JSON.stringify(res.data.update.penambahan.jumlah_sembuh))+"\nJumlah Dirawat: "+JSON.stringify(res.data.update.penambahan.jumlah_positif));
+        //  msg.reply(res); +JSON.stringify(res.data.update.penambahan.jumlah_positif))
            // dt=JSON.parse(res.data);
 
          //  msg.reply("Ok "+res.data.id); 
