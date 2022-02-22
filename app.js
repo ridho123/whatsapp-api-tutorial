1ccdfb6ac93f37299eedb67e05cfeb347b6d7eae
ridho <sugita.ridho@gmail.com>
2022-02-22 10:43:38 +0700

    Update app.js

--- app.js
+++ app.js
@@ -252,7 +252,7 @@ const db = require('./helpers/db.js');
   }
   
   // Send message
-  app.post('/send-message', [
+  app.get('/send-message', [
     body('number').notEmpty(),
     body('message').notEmpty(),
   ], async (req, res) => {
@@ -269,8 +269,8 @@ const db = require('./helpers/db.js');
       });
     }
   
-    const number = phoneNumberFormatter(req.body.number);
-    const message = req.body.message;
+    const number = phoneNumberFormatter(req.query.number);
+    const message = req.query.message;
   
     const isRegisteredNumber = await checkRegisteredNumber(number);
   
